9ad25681c4c557b27890743de2a19bd4b069f23a
Nahid Islam <nahidcrx@outlook.com>
2024-03-30 20:30:51 +0600

    Admin LTE Added

---
+++ rupsayar/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using PagedList;
+using rupsayar.Helper;
+using rupsayar.Models;
+using rupsayar.Models.VM;
+using rupsayar.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rupsayar.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IProductService _productService;
+        public AdminController(IProductService productService) 
+        {
+            _productService = productService;
+        }
+
+        public ActionResult CreateProduct() 
+        {
+            return View();
+        }
+
+    }
+}
