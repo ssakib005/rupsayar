57a7e2272d425f6f9d2a3b74afcec80e7a57f58c
Nahid Islam <nahidcrx@outlook.com>
2024-04-02 00:37:14 +0600

    Data annotation and validation added to CreateProduct

--- rupsayar/Models/VM/Tbl_Product_VM.cs
+++ rupsayar/Models/VM/Tbl_Product_VM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +9,21 @@ namespace rupsayar.Models.VM
     public class Tbl_Product_VM
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Category is required.")]
         public long Tbl_CategoryId { get; set; }
         public Tbl_Category Tbl_Category { get; set; }
+        [Required(ErrorMessage = "Unit Price is required.")]
         public float UnitPrice { get; set; }
+        [Required(ErrorMessage = "Quantity is required.")]
         public int Quantity { get; set; }
         public virtual List<Tbl_ProductVariant> Tbl_ProductVariants { get; set; }
         public bool IsNewArrival { get; set; }
         public virtual List<Tbl_ProductRate> Tbl_ProductRates { get; set; }
+        [Required(ErrorMessage = "Image is required.")]
         public virtual List<HttpPostedFileBase> Tbl_ProductImageList { get; set; }
         public virtual List<Tbl_ProductImages> Tbl_ProductImages { get; set; }
         public virtual List<Tbl_Product> RelatedProducts { get; set; }
